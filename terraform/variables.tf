1d76512dd2ae86e284d87c635a72804728176019
Uyên Thy <uynthyee.313@gmail.com>
2025-10-23 15:16:45 +0700

    feat(vpc): add base vpc and networks

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "Region for aws"
+  type = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name for my project"
+  type = string
+  default = "group9"
+}
