1228245cff2e432d04b3cc5e45f9f09db225535a
Uyên Thy <uynthyee.313@gmail.com>
2025-12-13 14:03:16 +0700

    feat(NAT, route table): add base NAT and route table

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,12 @@ module "vpc" {
   project_name = var.project_name
 }
 
+module "nat" {
+  source = "./modules/nat"
+  project_name = var.project_name
+  public_subnet_id = module.vpc.public_subnet_id
+}
+
 module "ec2" {
   source           = "./modules/ec2"
   project_name     = var.project_name
@@ -19,4 +25,14 @@ module "ec2" {
   instance_type    = "t3.micro"
   key_name         = var.ec2_key_name
   my_ip_cidr       = var.my_ip_cidr
+}
+
+module "route_table" {
+  source = "./modules/route_tables"
+  project_name     = var.project_name
+  vpc_id           = module.vpc.vpc_id
+  internet_gateway_id = module.vpc.internet_gateway_id
+  nat_gateway_id   = module.nat.nat_gateway_id
+  public_subnet_id = module.vpc.public_subnet_id
+  private_subnet_id = module.vpc.private_subnet_id
 }
