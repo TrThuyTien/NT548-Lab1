762b8c49161126aedc607f7675849f7eb45949e5
Uyên Thy <159131610+moeruuu@users.noreply.github.com>
2025-12-25 16:45:11 +0700

    Nat (#8)
    * EC2s and Security Groups
    
    * EC2s and Security Groups
    
    * feat(NAT, route table): add base NAT and route table
    
    ---------
    
    Co-authored-by: thuanthentran <thuanthentran@gmail.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,18 @@ variable "project_name" {
   type = string
   default = "group9"
 }
+
+variable "ec2_ami" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "ec2_key_name" {
+  description = "Name of AWS key pair"
+  type        = string
+}
+
+variable "my_ip_cidr" {
+  description = "Personal IP for SSH"
+  type        = string
+}
